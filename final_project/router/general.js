22d0dffee1636bc4a60a7fc4628ff8330f6c258a
MyriamAA <myriamabouatmeh@gmail.com>
2024-08-14 14:12:19 +0300

    Task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,7 +21,18 @@ public_users.get("/", function (req, res) {
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+
+  const isbn = req.params.isbn;
+  if (isNaN(isbn)) {
+    return res.status(400).json({ message: "Invalid ISBN format" });
+  }
+  const book = books[isbn];
+  console.log(book);
+  if (book) {
+    return res.status(200).json(book);
+  } else {
+    return res.send(404).json({ message: "No such book" });
+  }
 });
 
 // Get book details based on author

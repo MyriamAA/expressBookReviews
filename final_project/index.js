e49a6d7cc33b8cca075706a496bf512005f9ac34
MyriamAA <myriamabouatmeh@gmail.com>
2024-08-14 14:11:32 +0300

    Format index.js

--- final_project/index.js
+++ final_project/index.js
@@ -1,22 +1,45 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const session = require('express-session')
-const customer_routes = require('./router/auth_users.js').authenticated;
-const genl_routes = require('./router/general.js').general;
-
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const session = require("express-session");
+const customer_routes = require("./router/auth_users.js").authenticated;
+const genl_routes = require("./router/general.js").general;
+require("dotenv").config();
 const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use(
+  "/customer",
+  session({
+    secret: "fingerprint_customer",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+  // Extract token
+
+  const token = req.headers["authorization"];
+
+  if (!token) {
+    return res.status(403).send({ message: "Not authenticated" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, obj) => {
+    if (err) {
+      return res.status(401).send({ message: "Not authorized" });
+    }
+    req.user = obj;
+
+    next();
+  });
+  //Write the authenication mechanism here
 });
- 
-const PORT =5000;
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
